b58ec1ae7d6c9176165792f66d9e86de1b7b8ed1
Yusri Saad <yusryku@gmail.com>
2025-06-01 23:43:33 +0800

    fix6

--- routes/getQuizQuestion.js
+++ routes/getQuizQuestion.js
@@ -6,6 +6,9 @@ dotenv.config();
 
 const router = express.Router();
 
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("SUPABASE_SERVICE_ROLE_KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY);
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
