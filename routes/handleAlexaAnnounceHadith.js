05b24fd57549d3d982c7261bfecb3b25bc25cd6a
Yusri Saad <yusryku@gmail.com>
2025-07-05 12:23:04 +0800

    edit route

--- routes/handleAlexaAnnounceHadith.js
+++ routes/handleAlexaAnnounceHadith.js
@@ -5,28 +5,33 @@ dotenv.config();
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const { hadith } = req.body;
-
-  if (!hadith) {
-    return res.status(400).json({ error: "Hadith text is required." });
-  }
-
   try {
+    // Fetch dari API hadith
+    const fetchRes = await fetch("https://random-hadith-generator.vercel.app/muslim");
+    const { data } = await fetchRes.json();
+
+    const hadith = data.hadith_english?.replace(/\s+/g, " ").trim();
+    const header = data.header?.replace(/\s+/g, " ").trim();
+    const refno = data.refno;
+
     const response = {
       version: "1.0",
       response: {
         outputSpeech: {
           type: "PlainText",
-          text: `Here is a hadith for you: ${hadith}`,
+          text: `${header} ${hadith} (Hadith No: ${refno})`,
         },
         shouldEndSession: true,
       },
     };
+
     return res.json(response);
   } catch (error) {
-    console.error("Error processing hadith:", error);
+    console.error("Error fetching hadith:", error);
     return res.status(500).json({ error: "Hadith processing failed." });
   }
 });
+
+
 
 export default router;
